d8ba4b95d4976b469ca75b78ce566c44956735c9
shankygupta79 <shankygupta79@gmail.com>
2019-06-18 15:54:08 +0530

    HOST

--- db.js
+++ db.js
@@ -1,12 +1,7 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize('shopdb', 'shopper', 'shoppass', {
-    host: 'localhost',
-    dialect: 'mysql',
-    pool: {
-        min: 0,
-        max: 5,
-    }
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
 })
 
 const User = db.define('users', {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const SERVER_PORT=process.env.PORT || 333
+const SERVER_PORT=process.env.PORT || 3333
 const app = express();
 
 app.use(express.json())
@@ -9,4 +9,4 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/api', require('./routes/api/index.js').route)
 
-app.listen(SERVER_PORT, () => console.log('Server started at http://localhost:2678'))
+app.listen(SERVER_PORT, () => console.log('Server started at http://localhost:3333'))
