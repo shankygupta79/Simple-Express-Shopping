d1b2b48650e0631a08742c76a26234a94fea93dd
shankygupta79 <shankygupta79@gmail.com>
2019-06-18 12:40:47 +0530

    Add

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-
+const SERVER_PORT=process.env.PORT || 333
 const app = express();
 
 app.use(express.json())
@@ -9,4 +9,4 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/api', require('./routes/api/index.js').route)
 
-app.listen(2678, () => console.log('Server started at http://localhost:2678'))
+app.listen(SERVER_PORT, () => console.log('Server started at http://localhost:2678'))
